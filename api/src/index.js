64d65272ec65088339be4c4f3e78d3c82f38758c
Adnan Chowdhury <adnanchowdhury88@gmail.com>
2020-08-19 19:05:15 -0700

    Clean up

--- api/src/index.js
+++ api/src/index.js
@@ -4,8 +4,6 @@ import cors from '@koa/cors';
 import serve from 'koa-static';
 import graphqlServer from './graphql/server';
 
-import db from 'src/data';
-
 const PORT = process.env.PORT || 3000;
 const app = new Koa();
 const router = new Router();
