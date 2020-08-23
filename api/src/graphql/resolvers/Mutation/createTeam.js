73d22fc0c60e4ad5d97d7f64d5565c84a3713563
Adnan Chowdhury <bttf@users.noreply.github.com>
2020-08-22 23:21:09 -0700

    Create team mutation (#15)
    * Add createTeam backend mutation
    
    * Flesh out create team frontend mutation and UI

---
+++ api/src/graphql/resolvers/Mutation/createTeam.js
@@ -0,0 +1,59 @@
+import { nanoid } from 'nanoid';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import db from 'src/data';
+
+const ID_LEN = 6;
+
+async function _genPasswordDigest(password) {
+  return new Promise((resolve, reject) => bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      if (err) reject(err);
+      resolve(hash);
+    });
+  }));
+}
+
+async function createTeamMutation(_, args, context) {
+  const {
+    name,
+    password
+  } = args;
+
+  if (!name) {
+    return { errors: ['Name must be provided'] };
+  }
+
+  const existingTeam = await db.Team.findOne({
+    where: {
+      name
+    }
+  });
+
+  if (existingTeam) {
+    return { errors: ['Team name is taken'] };
+  }
+
+  // generate code
+  const code = nanoid(ID_LEN);
+
+  // create team
+  const team = await db.Team.create({
+    name,
+    code,
+  });
+
+  // create team settings
+  // - if password exists, generate password digest and persist
+  const passwordDigest = password ? await _genPasswordDigest(password) : undefined;
+
+  await db.TeamSettings.create({
+    password: passwordDigest,
+    team_id: team.id,
+  });
+
+  // return created team
+  return { createdTeam: team };
+}
+
+export default createTeamMutation;
--- api/src/graphql/typeDefs/Mutation.js
+++ api/src/graphql/typeDefs/Mutation.js
@@ -1,5 +1,12 @@
 import {gql} from 'apollo-server-koa';
 
 export default gql`
-  type Mutation {}
+  type CreateTeamMutationPayload {
+    createdTeam: Team
+    errors: [String!]
+  }
+
+  type Mutation {
+    createTeam(name: String!, password: String): CreateTeamMutationPayload!
+  }
 `;
