7eb8ec27ea880e95286cf4387393887a3d7388f8
bttf <adnanchowdhury88@gmail.com>
2020-08-23 01:12:00 -0700

    Add mutations

--- api/src/graphql/typeDefs/Mutation.js
+++ api/src/graphql/typeDefs/Mutation.js
@@ -6,7 +6,31 @@ export default gql`
     errors: [String!]
   }
 
+  type CreateStandupMutationPayload {
+    createdStandup: Standup
+    errors: [String!]
+  }
+
+  type CreateAbsenteeMutationPayload {
+    createdAbsentee: Absentee
+    errors: [String!]
+  }
+
+  type CreateFacilitatorMutationPayload {
+    createdFacilitator: Facilitator
+    errors: [String!]
+  }
+
+  type SetFacilitatorOrderMutationPayload {
+    facilitators: [Facilitator!]
+    errors: [String!]
+  }
+
   type Mutation {
     createTeam(name: String!, password: String): CreateTeamMutationPayload!
+    createStandup(date: String!, facilitatorUuid: String!): CreateStandupMutationPayload!
+    createAbsentee(facilitatorUuid: String!): CreateAbsenteeMutationPayload!
+    createFacilitator(name: String!, teamUuid: String!): CreateFacilitatorMutationPayload!
+    setFacilitatorOrder(uuids: [String!]!): SetFacilitatorOrderMutationPayload!
   }
 `;
