f1b70a77bea7a05b404aa6f0ab9708b04b84e09e
bttf <adnanchowdhury88@gmail.com>
2020-08-23 16:54:55 -0700

    Add clock

--- frontend/src/pages/Team/Clock.js
+++ frontend/src/pages/Team/Clock.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+import { Pane, Paragraph } from 'evergreen-ui';
+import './Clock.css';
 
 export default () => {
+  const [digits, setDigits] = useState('');
+
+  function tick() {
+    setDigits(format(new Date(), 'HH:mm:ss:SSS'));
+  }
+
+  useEffect(() => {
+    setInterval(tick, 5);
+  }, []);
+
   return (
-    <div>Clock</div>
+    <Pane className="clock-container">
+      <div className="quarter-year">
+        {format(new Date(), 'qqq yyyy')} - {format(new Date(), 'MMM do')}
+      </div>
+      <div className="clock">
+        {digits}
+      </div>
+    </Pane>
   );
 };
