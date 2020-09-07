fee085f3f57f19693e1eb9937cec12f1b8d5bc2f
bttf <adnanchowdhury88@gmail.com>
2020-09-07 13:54:35 -0700

    Add prod var for socket

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,2 +1,3 @@
 import io from 'socket.io-client';
-export default io('http://localhost:3000');
+const NODE_ENV = process.env.NODE_ENV
+export default io(NODE_ENV === 'production' ? 'https://api.standup.rocks' : 'http://localhost:3000');
